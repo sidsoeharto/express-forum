45ab668fe71687121cc8056cbb90b844726ce626
sidsoeharto <msidiq.15@gmail.com>
2020-11-20 09:07:48 +0700

    deploy ulang

--- controllers/postcontroller.js
+++ controllers/postcontroller.js
@@ -28,6 +28,60 @@ class PostController {
       res.send(err)
     })
   }
+
+  static showEditPost(req, res) {
+    const id = Number(req.params.id)
+    let dataSubforum;
+    Subforum.findAll()
+    .then(data => {
+      dataSubforum = data;
+      return Post.findByPk(id)
+    })
+    .then(data => {
+      res.render("postedit.ejs",{data, dataSubforum})
+    })
+    .catch(err => {
+      res.send(err)
+    })
+  }
+
+  static editPost(req, res) {
+    const id = Number(req.params.id)
+
+    const editPost = {
+      title: req.body.title,
+      content: req.body.content,
+      SubforumId: req.body.SubforumId
+    }
+
+    Post.update(editPost, {
+      where:{
+        id: id
+      }
+    })
+    .then(data => {
+      res.redirect('/')
+    })
+    .catch(err => {
+      res.send(err)
+    })
+  }
+
+  static deletePost(req,res) {
+    const id = Number(req.params.id)
+
+    Post.destroy({
+      where: {
+        id: id
+      }
+    })
+    .then(data => {
+      res.redirect('/')
+    })
+    .catch(err => {
+      res.send(err)
+    })
+  }
 }
 
 module.exports = PostController
--- routes/post.js
+++ routes/post.js
@@ -7,10 +7,11 @@ const PostController = require("../controllers/postcontroller")
 router.get('/post', PostController.showCreatePost)
 
 router.post('/post', PostController.createPost)
-//router.get('/', PostController.showAllSubforums)
 
-// router.get('/:subforum', SubforumController.showSubforum)
+router.get('/post/edit/:id', PostController.showEditPost)
 
-// router.get('/:subforum/:id', SubforumController.getPost)
+router.post('/post/edit/:id', PostController.editPost)
+
+router.delete('/post/delete/:id', PostController.deletePost)
 
 module.exports = router
